8b10cab7d690ed7722d91701ed4631ab33fb81b0
zenon7171 <zenon.sns@gmail.com>
2024-12-29 02:10:51 +0900

    Update killer_bot.py

--- killer_bot.py
+++ killer_bot.py
@@ -41,7 +41,7 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8000)
 
 def keep_alive():
     t = Thread(target=run)
